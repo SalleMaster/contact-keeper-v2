fd0e9a49c9b432f5354b3af8b939b7185ba29b5f
Sasa Radovanovic <salle90ar@gmail.com>
2019-10-23 12:13:15 +0200

    Private Route Component

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alerts from './components/layout/Alerts';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
@@ -26,13 +27,13 @@ const App = () => {
           <Router>
             <Fragment>
               <Navbar />
-              <div className='container'>
+              <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path='/' component={Home} />
-                  <Route exact path='/about' component={About} />
-                  <Route exact path='/register' component={Register} />
-                  <Route exact path='/login' component={Login} />
+                  <PrivateRoute exact path="/" component={Home} />
+                  <Route exact path="/about" component={About} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
                 </Switch>
               </div>
             </Fragment>
--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -12,6 +12,7 @@ import {
 export default (state, action) => {
   switch (action.type) {
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', action.payload.token);
       return {
         ...state,
@@ -21,13 +22,15 @@ export default (state, action) => {
       };
 
     case REGISTER_FAIL:
+    case LOGIN_FAIL:
     case AUTH_ERROR:
+    case LOGOUT:
       localStorage.removeItem('token');
       return {
         ...state,
         token: null,
         isAuthenticated: false,
-        loading: true,
+        loading: false,
         user: null,
         error: action.payload
       };
