7c346d3e28d81dbe733e55b9e29980953939b78e
Sasa Radovanovic <salle90ar@gmail.com>
2019-11-11 15:54:43 +0100

    Add Gallery Item -images-

--- client/src/components/gallery/AddGalleryItem.js
+++ client/src/components/gallery/AddGalleryItem.js
@@ -16,6 +16,9 @@ const AddGalleryItem = () => {
     images: ''
   });
 
+  const [mainImageName, setMainImageName] = useState('Choose file');
+  const [imagesName, setImagesName] = useState('Choose file');
+
   const { name, category, description, price, mainImage, images } = galleryItem;
 
   const onChange = e => {
@@ -30,22 +33,57 @@ const AddGalleryItem = () => {
       ...galleryItem,
       mainImage: e.target.files[0]
     });
+    setMainImageName(e.target.files[0].name);
+  };
+
+  const onChangeImages = e => {
+    setGalleryItem({
+      ...galleryItem,
+      images: [...e.target.files]
+    });
+
+    let imageNames = [];
+    const filesLength = e.target.files.length;
+    for (let i = 0; i < filesLength; i++) {
+      imageNames.push(e.target.files[i].name);
+    }
+    setImagesName(imageNames);
   };
 
   const onSubmit = e => {
     e.preventDefault();
     addGalleryItem(galleryItem);
+    setGalleryItem({
+      name: '',
+      category: '',
+      description: '',
+      price: '',
+      mainImage: '',
+      images: ''
+    });
+  };
+
+  const onClose = () => {
+    setGalleryItem({
+      name: '',
+      category: '',
+      description: '',
+      price: '',
+      mainImage: '',
+      images: ''
+    });
+    setMainImageName('Choose File');
   };
 
   return (
     <Fragment>
       {/* ADD MODAL BUTTON */}
-      <div className='row'>
-        <div className='col m-right'>
+      <div className="row">
+        <div className="col m-right">
           <button
-            className='btn btn-primary'
-            data-toggle='modal'
-            data-target='#addGalleryItem'
+            className="btn btn-primary"
+            data-toggle="modal"
+            data-target="#addGalleryItem"
           >
             Add Gallery Item
           </button>
@@ -53,110 +91,111 @@ const AddGalleryItem = () => {
       </div>
 
       {/* MODAL */}
-      <div className='modal' id='addGalleryItem'>
-        <div className='modal-dialog'>
-          <div className='modal-content'>
-            <div className='modal-header'>
-              <h5 className='modal-title'>Add Gallery Item</h5>
-              <button className='close' data-dismiss='modal'>
-                &times;
+      <div className="modal" id="addGalleryItem">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Add Gallery Item</h5>
+              <button className="close" data-dismiss="modal">
+                <span onClick={onClose}>&times;</span>
               </button>
             </div>
-            <div className='modal-body'>
+            <div className="modal-body">
               <form onSubmit={onSubmit}>
-                <div className='form-group'>
-                  <label htmlFor='name'>Name</label>
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
                   <input
-                    name='name'
-                    type='text'
+                    name="name"
+                    type="text"
                     placeholder={name !== '' ? name : 'Name'}
-                    className='form-control'
+                    className="form-control"
                     onChange={onChange}
+                    required
                   />
                 </div>
-                <div className='form-group'>
-                  <label htmlFor='category'>Category</label>
+                <div className="form-group">
+                  <label htmlFor="category">Category</label>
                   <select
-                    className='form-control'
-                    id='category'
-                    name='category'
+                    className="form-control"
+                    id="category"
+                    name="category"
                     onChange={onChange}
+                    required
                   >
-                    <option value=''>--</option>
-                    <option value='Action'>Action</option>
-                    <option value='Pixel Art'>Pixel Art</option>
-                    <option value='RPG'>RPG</option>
+                    <option value="">--</option>
+                    <option value="Action">Action</option>
+                    <option value="Pixel Art">Pixel Art</option>
+                    <option value="RPG">RPG</option>
                   </select>
                 </div>
-                <div className='form-group'>
-                  <label htmlFor='description'>Description</label>
+                <div className="form-group">
+                  <label htmlFor="description">Description</label>
                   <textarea
-                    name='description'
-                    cols='30'
-                    rows='10'
-                    placeholder='Description'
-                    className='form-control'
+                    name="description"
+                    cols="30"
+                    rows="10"
+                    placeholder="Description"
+                    className="form-control"
                     onChange={onChange}
+                    required
                   ></textarea>
                 </div>
-                <div className='form-group'>
-                  <label htmlFor='price'>Price</label>
+                <div className="form-group">
+                  <label htmlFor="price">Price</label>
                   <input
-                    name='price'
-                    type='text'
-                    placeholder='Price'
-                    className='form-control'
+                    name="price"
+                    type="text"
+                    placeholder="Price"
+                    className="form-control"
                     onChange={onChange}
+                    required
                   />
                 </div>
-                <div className='form-group'>
-                  <label htmlFor='mainImage'>Insert Main Image</label>
-                  <div className='custom-file'>
+                <div className="form-group">
+                  <label htmlFor="mainImage">Insert Main Image</label>
+                  <div className="custom-file">
                     <input
-                      type='file'
-                      id='myfile'
-                      name='mainImage'
-                      className='custom-file-input'
+                      type="file"
+                      id="myfile"
+                      name="mainImage"
+                      className="custom-file-input"
                       onChange={onChangeMainImage}
+                      required
                     />
-                    <label className='custom-file-label' htmlFor='myfile'>
-                      Choose file
+                    <label className="custom-file-label" htmlFor="myfile">
+                      {mainImageName}
                     </label>
                   </div>
                 </div>
-                <div className='form-group'>
-                  <label htmlFor='images'>Insert Slide Images</label>
-                  <div className='custom-file'>
+                <div className="form-group">
+                  <label htmlFor="images">Insert Slide Images</label>
+                  <div className="custom-file">
                     <input
-                      type='file'
-                      id='slideImages'
-                      name='images'
-                      className='custom-file-input'
+                      type="file"
+                      id="slideImages"
+                      name="images"
+                      className="custom-file-input"
                       multiple
+                      onChange={onChangeImages}
                     />
-                    <label className='custom-file-label' htmlFor='slideImages'>
-                      Choose files
+                    <label className="custom-file-label" htmlFor="slideImages">
+                      {imagesName === 'Choose file'
+                        ? 'Choose file'
+                        : imagesName.map((image, index) => (
+                            <span key={index}>{image} </span>
+                          ))}
                     </label>
                   </div>
                 </div>
-                <div className='modal-footer'>
+                <div className="modal-footer">
                   <input
-                    type='submit'
-                    value='Upload'
-                    className='btn btn-primary btn-block mt-4'
+                    type="submit"
+                    value="Upload"
+                    className="btn btn-primary btn-block mt-4"
                   />
                 </div>
               </form>
             </div>
-            {/* <div className='modal-footer'>
-              <input
-                type='submit'
-                className='btn btn-primary'
-                data-dismiss='modal'
-                value='Submit'
-                onClick={onSubmit}
-              />
-            </div> */}
           </div>
         </div>
       </div>
