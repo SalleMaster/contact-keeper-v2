d736f4e838d7c0959b6a2f45fec8f83ec5708d1f
Sasa Radovanovic <salle90ar@gmail.com>
2020-03-09 21:36:03 +0100

    aj sad

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === 'production') {
   //make uploads directory static
   app.use('/uploads', express.static('uploads'));
 
-  app.get('*', (req, res) =>
+  app.get('/', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
