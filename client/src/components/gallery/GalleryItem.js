7cf8338afdc963a36c2d8a77c29942a441cb81c8
Sasa Radovanovic <salle90ar@gmail.com>
2020-03-09 13:51:19 +0100

    Messenger plugin added

--- client/src/components/gallery/GalleryItem.js
+++ client/src/components/gallery/GalleryItem.js
@@ -1,8 +1,8 @@
-import React, { Fragment, useContext } from "react";
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-import GalleryContext from "../../context/gallery/galleryContext";
-import DeleteButton from "./DeleteButton";
+import React, { Fragment, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import GalleryContext from '../../context/gallery/galleryContext';
+import DeleteButton from './DeleteButton';
 
 const GalleryItem = ({ galleryItem }) => {
   const galleryContext = useContext(GalleryContext);
@@ -18,36 +18,41 @@ const GalleryItem = ({ galleryItem }) => {
     _id
   } = galleryItem;
 
-  let badgeStyle = "";
+  let badgeStyle = '';
 
-  if (category === "RPG") {
-    badgeStyle = "badge-danger";
+  if (category === 'RPG') {
+    badgeStyle = 'badge-danger';
   }
-  if (category === "Pixel Art") {
-    badgeStyle = "badge-primary";
+  if (category === 'Pixel Art') {
+    badgeStyle = 'badge-primary';
   }
-  if (category === "Action") {
-    badgeStyle = "badge-warning";
+  if (category === 'Action') {
+    badgeStyle = 'badge-warning';
   }
 
   const onClick = () => {
     setCurrent(galleryItem);
-    localStorage.setItem("current", JSON.stringify(galleryItem));
+    localStorage.setItem('current', JSON.stringify(galleryItem));
   };
 
   return (
     <Fragment>
-      <div className="card">
-        <img className="card-img-top" src={`/uploads/${mainImage}`} alt="" />
-        <div className="card-body text-center d-flex flex-column justify-content-between">
-          <h4 className="card-title">{name}</h4>
-          <p className="card-text text-left">
+      <div className='card'>
+        {/* <img className='card-img-top' src={`/uploads/${mainImage}`} alt='' /> */}
+        <img
+          className='card-img-top'
+          src={`${process.env.PUBLIC_URL}/uploads/${mainImage}`}
+          alt=''
+        />
+        <div className='card-body text-center d-flex flex-column justify-content-between'>
+          <h4 className='card-title'>{name}</h4>
+          <p className='card-text text-left'>
             <span className={`badge ${badgeStyle} p-2`}>{category}</span>
           </p>
           <div>
             <Link
-              className="btn btn-success btn-block"
-              to="/gallery/read-more"
+              className='btn btn-success btn-block'
+              to='/gallery/read-more'
               onClick={onClick}
             >
               Read More
